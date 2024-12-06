b04e9f771c07bebbf6b04ee514c4ba70732ac1c6
XForYouX <52589961+ImNoobUser@users.noreply.github.com>
2024-12-06 15:13:08 +0700

    Fix Error Lost Connection After Install Requests

--- autoinstall.sh
+++ autoinstall.sh
@@ -5,7 +5,7 @@ termux-setup-storage -y
 pkg install python -y
 pkg install python-pip -y
 
-pip install requests 
+pip install --proxy=user:pass@server:port requests
 pip install bs4 
 pip install colorama 
 pip install pyfiglet 
