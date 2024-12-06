321e48ff13708df553fcb95a6c6e42d9bcf4286b
XForYouX <52589961+ImNoobUser@users.noreply.github.com>
2024-12-06 10:46:20 +0700

    .

--- autoinstall.sh
+++ autoinstall.sh
@@ -5,6 +5,6 @@ termux-setup-storage -y
 pkg install python -y
 pkg install python-pip -y
 
-pip install rich requests bs4 colorama pyfiglet pystyle
+pip install requests bs4 colorama pyfiglet pystyle rich
 
 
