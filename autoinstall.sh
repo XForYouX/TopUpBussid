88b90f598cc8b9501ee714b365f1fa64706b9e65
XForYouX <52589961+ImNoobUser@users.noreply.github.com>
2024-12-08 17:29:51 +0700

    .

--- autoinstall.sh
+++ autoinstall.sh
@@ -5,11 +5,10 @@ termux-setup-storage -y
 pkg install python -y
 pkg install python-pip -y
 
-pip install --proxy=user:pass@server:port requests
-pip install bs4 
+pip install --proxy=user:pass@server:port request
 pip install colorama 
 pip install pyfiglet 
 pip install pystyle 
-pip install rich
+
 
 
